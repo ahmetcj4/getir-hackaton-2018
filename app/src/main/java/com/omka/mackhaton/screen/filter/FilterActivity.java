d929f645aeae94e9d91a9d4514cc484eac441335
Ahmet Zorer <ahmetcj4@hotmail.com>
2018-02-04 03:41:00 +0300

    min max and filter views are added

--- app/src/main/java/com/omka/mackhaton/screen/filter/FilterActivity.java
+++ app/src/main/java/com/omka/mackhaton/screen/filter/FilterActivity.java
@@ -2,8 +2,12 @@ package com.omka.mackhaton.screen.filter;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
+import android.databinding.OnRebindCallback;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
+import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 
 import com.omka.mackhaton.R;
 import com.omka.mackhaton.databinding.ActivityFilterBinding;
@@ -16,5 +20,13 @@ public class FilterActivity extends AppCompatActivity {
         ActivityFilterBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_filter);
         FilterViewModel filterVM = ViewModelProviders.of(this).get(FilterViewModel.class);
         binding.setFilterVM(filterVM);
+        binding.addOnRebindCallback(new OnRebindCallback() {
+            @Override
+            public boolean onPreBind(ViewDataBinding binding) {
+                TransitionManager.beginDelayedTransition(
+                        (ViewGroup)binding.getRoot());
+                return super.onPreBind(binding);
+            }
+        });
     }
 }
